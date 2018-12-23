ba5ac9c877e8fb017134afeaa3e7b883244ef22a
Tajini <Sinso7Iblis@hotmail.fr>
2018-12-24 00:26:24 +0100

    Fix (App) : Several fixes around the application

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import StackNavigator from './Apps/Config/Routes'
+
+export default class App extends React.Component {
+  render() {
+
+    return (
+          <StackNavigator />
+    )
+  }
+}
--- Apps/Config/Routes.js
+++ Apps/Config/Routes.js
@@ -3,14 +3,11 @@ import {
   createAppContainer
 } from 'react-navigation';
 
-import {
-  AppRegistry
-} from 'react-native';
 
 //Chemins jeux 
 import  HomeScreen from '../Morpion/HomeScreen';
 import  GameScreen from '../Morpion/GameScreen';
-import HomeApp from '../../HomeApp';
+import HomeApp from '../HomeApp';
 import AppFlappy from '../Flappy/AppFlappy';
 
 const RootStack = createStackNavigator({ //Routes stacksnav
@@ -18,13 +15,8 @@ const RootStack = createStackNavigator({ //Routes stacksnav
     AppFlappy: {screen: AppFlappy},
 	  Home: { screen: HomeScreen },
 	  Game: { screen: GameScreen }
-
-
-
 });
+
 const App = createAppContainer(RootStack);
 export default App;
 
-
-
-AppRegistry.registerComponent('Game', () => App);
